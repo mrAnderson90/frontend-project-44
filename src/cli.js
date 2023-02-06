a7cd669ce91655b5ee24052dec2e30cbd82df87d
mrAnderson90 <tyler.andreev@mail.ru>
2023-02-06 19:02:42 +0300

    Fix js files

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-	const name = readlineSync.question('May I have your name? ');
-	return `Hello, ${name}!`;
+  const name = readlineSync.question('May I have your name? ');
+  return `Hello, ${name}!`;
 };
 
 export default greeting;
